83da38858346fe9efde85a37e235184ab70790f4
Reynoso <reynoso.geronimo@gmail.com>
2023-10-10 16:51:40 -0300

    animaciones y mas clientes

--- public/js/functions.js
+++ public/js/functions.js
@@ -24,6 +24,21 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('hola')
      subMenu.classList.toggle("open-menu")
     })
-    
+    function reveal() {
+        let reveals = document.querySelectorAll(".reveal");
+      
+        for (let i = 0; i < reveals.length; i++) {
+          let windowHeight = window.innerHeight;
+          let elementTop = reveals[i].getBoundingClientRect().top;
+          let elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          }
+        }
+      }
+      
+      window.addEventListener("scroll", reveal);
+      
     
 })
