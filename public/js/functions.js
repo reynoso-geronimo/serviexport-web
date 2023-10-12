32be902fdc460efaa0ef77861a1739e4752784de
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-10-11 22:16:43 -0300

    mejoras

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,47 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const swiper = new Swiper(".mySwiper", {
-        pagination: {
-            el: ".swiper-pagination",
-            type: "progressbar",
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        loop: true,
-    });
+  const swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
 
 
-    const subMenu = document.getElementById("subMenu")
+  const subMenu = document.getElementById("subMenu")
 
-    const menu = document.querySelector(".fa-bars")
-   
-    menu.addEventListener('click',()=>{
-        console.log('hola')
-     subMenu.classList.toggle("open-menu")
-    })
-    function reveal() {
-        let reveals = document.querySelectorAll(".reveal");
-      
-        for (let i = 0; i < reveals.length; i++) {
-          let windowHeight = window.innerHeight;
-          let elementTop = reveals[i].getBoundingClientRect().top;
-          let elementVisible = 150;
-      
-          if (elementTop < (windowHeight - elementVisible)) {
-           
-            reveals[i].classList.add("active");
-          }
-        }
+  const menu = document.querySelector(".fa-bars")
+
+  menu.addEventListener('click', () => {
+    console.log('hola')
+    subMenu.classList.toggle("open-menu")
+  })
+  function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+
+      if (elementTop < (windowHeight - elementVisible)) {
+
+        reveals[i].classList.add("active");
       }
-      
-      window.addEventListener("scroll", reveal);
-      if(window.innerHeight>1000){
-        reveal()
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+  if (window.innerHeight > 1000) {
+    reveal()
+  }
+
+  function zoom() {
+    let logosClientes = document.querySelectorAll(".logo-cliente");
+    let clientes= document.querySelectorAll('.cliente')
+    for (let i = 0; i < logosClientes.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = clientes[i].getBoundingClientRect().top;
+      let elementVisible = 350;
+
+      if (elementTop - (windowHeight - elementVisible)<0 && elementTop - (windowHeight - elementVisible)>-150) {
+        console.log('hi');
+        logosClientes[i].classList.add("zoom");
+      }else{
+        logosClientes[i].classList.remove("zoom");
       }
-    
+    }
+  }
+
+  window.addEventListener("scroll", zoom);
+  if (window.innerHeight > 1000) {
+    zoom()
+  }
+
 })
